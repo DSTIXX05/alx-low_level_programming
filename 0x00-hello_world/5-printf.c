2e2ce7e3eda3f64e9a24f2bc125068c89d49168b
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-09 16:49:40 -0700

    thankk God for Ibinabo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: 'not making that mistake again'
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
