3f0f49fb5286a499015546bdaf75915f66e384cc
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-09 16:34:59 -0700

    thank you IB

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/**
- * *main
- * * 
- * *Return: Always 0 (Success)
- */
+/** 
+*main - This prints strings
+*
+* Description: 'this still prints strings'
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
