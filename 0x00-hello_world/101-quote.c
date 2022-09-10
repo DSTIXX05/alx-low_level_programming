d8ef2312f9dac8c7b2b5485a603d016aa36aec64
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-09 18:04:54 -0700

    101 code dot c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description - 'prints some random ass strings'
+ * Return: 1 - this is new
+*/
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1)
+}
