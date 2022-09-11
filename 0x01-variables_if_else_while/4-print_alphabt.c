e07862469e516acd49ec356f850df9cd0e2f4e96
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-11 16:11:57 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.>
+
+/*
+ *main: The program starts here.
+ *
+ * Description: This program prints evey lowercase letter except q and e
+ *
+ * Return: 0 as usual
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
