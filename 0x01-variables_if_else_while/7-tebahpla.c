b15ef33aae3a8785de193b10d137cafd521e6235
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-11 17:05:28 -0700

    7-tebahpla.c

---
+++ 0x01-variables_if_else_while/7-tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all the lowercase alphabets in reverse
+ *
+ * Return: 0 as usual.
+ *
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; char >= a; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
