60fa042186dc27a66bcfb41579c9adaa50288043
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-13 20:20:32 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main"
+
+/**
+ * print_to_98 - This function prints all numbers from n to 98
+ * from n to 98, followed by a new line
+ * @n: Starting integer to count down or up to 98
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%i, ", i);
+			else
+				printf("%i", i);
+		}
+
+
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+				printf("%i, ", i);
+			else
+				printf("%i", i);
+		}
+
+	}
+	else
+		printf("%i", n);
+
+	printf("\n")
+
+
+}
