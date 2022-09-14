cae5434d33fa78253a3f2d6677f65aa7d4f90bf9
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-13 19:05:14 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - this function prints every minute of the day of Jack Bauer, starting from 00: 00 to 23: 59
+ * jack_bauer - this function is a minute counter
  * Return: 0 normally
  */
 void jack_bauer(void)
