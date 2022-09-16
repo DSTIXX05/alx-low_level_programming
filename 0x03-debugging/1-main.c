ad0c03c41f71b16659e54998bfd8c09090e02e2b
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-15 17:22:45 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - causes an infinite loop
@@ -20,5 +21,5 @@ int main(void)
 */
 	 printf("Infinite loop avoided! \\o/\n");
 
-	return (0);
+	 return (0);
 }
