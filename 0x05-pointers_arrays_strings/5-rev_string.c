51b6e67d401b42e2e139b21a9dc808183505beea
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-19 17:39:47 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * void rev_string - prints strings in reverse
+ * @*s: string argument
+ *
+ * return void
+ */
+
+void rev_string(char *s)
+{
+
+	int i = 0, len = 0;
+	char tmp;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len -i -1];
+		s[len - i - 1] = tmp;
+	}
+
+
+}
