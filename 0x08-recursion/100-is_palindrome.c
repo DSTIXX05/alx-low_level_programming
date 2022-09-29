5382959d9e5ca43072053c44cffd4892d37e8dba
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2022-09-28 19:31:46 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -30,7 +30,7 @@ int comparator(char *s, int n1, int n2)
 		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
-
+}
 /**
  * is_palindrome - detects if a string is a palindrome.
  *
@@ -40,7 +40,9 @@ int comparator(char *s, int n1, int n2)
  */
 
 int is_palindrome(char *s)
+{
 	if (*s == '\0')
 		return (1);
 	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
+
