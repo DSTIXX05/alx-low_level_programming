c78fc132c494576456ca82b88277e8a807bd4777
DSTIXX)% <delightsomedunsiasolo24@gmail.com>
2023-02-24 12:53:41 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: about printing single digit numbers as CSVs
- *
- * Return: 0 as always
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 0; num < 10; num++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-
-		putchar(',');
-		putchar(' ');
-
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
